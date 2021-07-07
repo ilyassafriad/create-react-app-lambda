2747150c9b6e287b9a5cd829b16cea3393a7f8c9
ilyass afriad <78428723+ilyassafriad@users.noreply.github.com>
2021-07-08 00:35:37 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,50 +1,52 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React from 'react' ;
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom' ;
+import './App.css' ;
+import Home from './components/Home' ;
+import Book1 from './components/Book1' ;
+import {FontAwesome} from 'react-icons/fa' ;
+import {FaArrowRight} from 'react-icons/fa';
+import Book2 from './components/Book2';
+import Book3 from './components/Book3';
+import Book4 from './components/Book4';
+import Contact from './components/Contact' ;
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
 
-  handleClick = api => e => {
-    e.preventDefault()
+class App extends React.Component{
+  
+  render(){
+    const NavBar = ()=>{
+      return(
+        <main className="navbar">
+        
+          <ul>
+            <li><Link className="link" to="/" id="active">FreeBusinessBooks </Link></li>
+            <li><Link className="link" to="/book1">Book 1<FaArrowRight/> </Link></li>
+            <li><Link className="link" to="/book2">Book 2<FaArrowRight/> </Link></li>
+            <li><Link className="link" to="/book3">Book 3<FaArrowRight/> </Link></li>
+            <li><Link className="link" to="/book4">Book 4<FaArrowRight/> </Link></li>
+            <li><Link className="link" to="/contact">Contact Use<FaArrowRight/> </Link></li>
+          </ul>
+        </main>
+      )
+    }
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+    return(
+      <div style={{backgroundColor: "#1D1D1D"}}>
+        <main>
+          <BrowserRouter>
+            <NavBar />
+            <Route path="/" component={Home} exact/>
+            <Route path="/book1" component={Book1} />
+            <Route path="/book2" component={Book2} />
+            <Route path="/book3" component={Book3} />
+            <Route path="/book4" component={Book4} />
+            <Route path="/contact" component={Contact} />
+          </BrowserRouter>
+        </main>
       </div>
     )
   }
 }
 
-export default App
+
+export default App ;
